dc4890e2d1da44ce5a5200aeaf2bebfd5d9d4ef1
radiah23 <khantasnimm3345@gmail.com>
2025-12-04 01:46:12 -0500

    Initial file

---
+++ NGOReportR/R/import.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @import dplyr
+#' @import ggplot2
+#' @import skimr
+#' @import cli
+#' @import patchwork
+#' @importFrom performance model_performance check_model
+#' @importFrom parameters parameters
+#' @importFrom report report
+#' @importFrom modelsummary modelsummary
+#' @importFrom gtsummary tbl_regression
+#' @importFrom glue glue
+
+NULL
---
+++ NGOReportR/R/validate_data.R
@@ -0,0 +1,11 @@
+#' Validate the data condition in the uploaded dataset
+#' @param data A data frame containing the uploaded dataset.
+#' @return A list containing a boolean 'is_valid' and a character vector 'messages
+#' @return Summary of the dataset
+
+#'@export
+  validate_data <- function(data) {
+    cli::cli_h1("Validation")
+    skim_summary <- skimr::skim(data)
+    print(skim_summary)
+  }
